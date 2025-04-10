f93706fe1573452a426c4b18bce621445d157b12
DAQEM <daqemyt@gmail.com>
2025-04-10 15:56:16 +0200

    Added environment variables to config.

--- ApiGateway/Program.cs
+++ ApiGateway/Program.cs
@@ -3,7 +3,7 @@ using Ocelot.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
+builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true).AddEnvironmentVariables();
 builder.Services.AddOcelot(builder.Configuration);
 
 builder.Services.AddControllers();
